92142eb3da995ae42bfb291d3c15b20d376b3f2f
etvoilajw <etvoila.jw@gmail.com>
2023-04-10 23:02:09 +1200

    Add project files.

---
+++ TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    /// <summary>
+    /// Represents the prize
+    /// </summary>
+    public class PrizeModel
+    {
+        /// <summary>
+        /// Represents the place of the prize
+        /// </summary>
+        public int PlaceNumber { get; set; }
+
+        /// <summary>
+        /// Represents the name of the prize place
+        /// </summary>
+        public string PlaceName { get; set; }
+
+        /// <summary>
+        /// Represents the amount the prize
+        /// </summary>
+        public decimal PrizeAmount { get; set; }
+
+        /// <summary>
+        /// Represents the percentage of the prize
+        /// </summary>
+        public double PrizePercentage { get; set; }
+    }
+}
