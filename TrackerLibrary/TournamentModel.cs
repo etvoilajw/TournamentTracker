07c1f9406a80d8a893a8b7382cbf9dde30f0dfe0
etvoilajw <etvoila.jw@gmail.com>
2023-04-10 23:13:19 +1200

    add comments for TournamentModel

--- TrackerLibrary/TournamentModel.cs
+++ TrackerLibrary/TournamentModel.cs
@@ -6,12 +6,34 @@ using System.Threading.Tasks;
 
 namespace TrackerLibrary
 {
+    /// <summary>
+    /// Represents the tournament
+    /// </summary>
     public class TournamentModel
     {
+        /// <summary>
+        /// Represents the tournament's name
+        /// </summary>
         public string TournamentName { get; set; }
+
+        /// <summary>
+        /// Represents the Entry fee for this tournament
+        /// </summary>
         public decimal EntryFee { get; set; }
+
+        /// <summary>
+        /// Represents the list of teams entered in this tournament
+        /// </summary>
         public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+
+        /// <summary>
+        /// Represents the list of prizes for this tournament
+        /// </summary>
         public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+        
+        /// <summary>
+        /// Represents the list of rounds in this tournament
+        /// </summary>
         public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>>();
     }
 }
